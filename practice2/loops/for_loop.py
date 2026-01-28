2e3bb0423ba4b68901ac95a9bd989f3b102d638d
darkenov <skilbot15@gmail.com>
2026-01-28 21:39:51 +0500

    for loop examples

---
+++ practice2/loops/for_loop.py
@@ -0,0 +1,18 @@
+for x in "banana":
+  print(x)
+
+for i in range(10):
+  print(i)
+
+mytuple = ("apple", "banana", "cherry")
+for x in mytuple:
+  print(x)
+
+mystr = "banana"
+for x in mystr:
+  print(x)
+
+a=12
+for i in range(a):
+    if i%2==0:
+        print(i)
