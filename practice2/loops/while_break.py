f3213293fd7200fe16dc89ceefab47374d4da53c
darkenov <skilbot15@gmail.com>
2026-01-28 23:23:56 +0500

    change examples while

--- practice2/loops/while_break.py
+++ practice2/loops/while_break.py
@@ -1,4 +1,4 @@
-i = 0
+i = 1
 while i < 3:
     w = input()
     if w == "0":
@@ -15,7 +15,7 @@ while True:
     s += x
 print(s)
 
-i = 1
+i = 2
 while i <= 100:
     if i % 7 == 0:
         print(i)
