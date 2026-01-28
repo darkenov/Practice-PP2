e16f94ab5a133d5b6cc06ffab5011110974eb293
darkenov <skilbot15@gmail.com>
2026-01-28 23:15:03 +0500

    while loops examples

---
+++ practice2/loops/while_loop.py
@@ -0,0 +1,28 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
+n = int(input())
+
+p = 1
+while p <= n:
+    print(p, end=" ")
+    p *= 2
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
