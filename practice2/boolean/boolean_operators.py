db3b181873b591bc65ff30d635fd1bf70d061da6
darkenov <skilbot15@gmail.com>
2026-01-28 13:55:07 +0500

    Examples operators

--- practice2/boolean/boolean_operators.py
+++ practice2/boolean/boolean_operators.py
@@ -10,5 +10,22 @@ print(not(z > 3 and z < 10))
 d=5
 print(not(d>10 or d>4))
 
-f=5
-print(not(f>6 or f>7))
+f = ["apple", "banana"]
+u = ["apple", "banana"]
+k = f
+print(f is k)
+print(f is u)
+print(f == u)
+
+text = "Hello World"
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
+
+print(6 & 3)
+print(6 | 3)
+print(6 ^ 3)
+
+#Multiplication * has higher precedence than addition +, and therefore multiplications are evaluated before additions:
+
+print(100 + 5 * 3)
